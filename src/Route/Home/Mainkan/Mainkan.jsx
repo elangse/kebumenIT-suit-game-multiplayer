9a0573f3a6543f38df616ad670aa50b9555f3d36
AlbabRiziq <albabriziq@gmail.com>
2023-01-14 16:00:06 +0700

    update

--- src/Route/Home/Mainkan/Mainkan.jsx
+++ src/Route/Home/Mainkan/Mainkan.jsx
@@ -1,7 +1,27 @@
 function Mainkan() {
   return (
-    <div>
-      <h1>Mainkan</h1>
+    <div className="w-screen h-screen flex flex-col bg-[#ff7b54] items-center justify-center">
+      <div className="border w-full h-52 flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <div className="flex">
+            <div className="border h-16 rounded-full w-16 m-2"></div>
+            <div className="border h-16 rounded-full w-16 m-2"></div>
+            <div className="border h-16 rounded-full w-16 m-2"></div>
+          </div>
+          <div className="border h-16 rounded-full w-16 m-2"></div>
+        </div>
+      </div>
+      <div className="border w-full h-52 flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <div className="border h-16 rounded-full w-16 m-2"></div>
+
+          <div className="flex">
+            <div className="border h-16 rounded-full w-16 m-2"></div>
+            <div className="border h-16 rounded-full w-16 m-2"></div>
+            <div className="border h-16 rounded-full w-16 m-2"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
